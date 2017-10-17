da12aef3af2145092aad15e2896f1a9b8ff480dd
kevin <kevin.nin92@gmail.com>
2017-10-18 00:10:12 +0530

    config file for server

--- web/config.js
+++ web/config.js
@@ -1,7 +1,18 @@
-'use strict'
+"use strict";
+
+const joi = require("joi");
+
+const envObjSchema = joi
+  .object({
+    PORT: joi.number().integer().min(0).max(65535).required()
+  })
+  .unknown(false)
+  .required();
+
+const envObj = joi.attempt(process.env, envObjSchema);
 
 const config = {
-  port: 0
-}
+  port: envObj.PORT
+};
 
-module.exports = config
+module.exports = config;
